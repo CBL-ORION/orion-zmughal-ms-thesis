af2ca51612dd64f7e3d2d8ee8879631503f63d91
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-11-30 07:54:55 -0600

    add dep files (gfx, preamble)

---
+++ gfx/present/go.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+CURDIR=`dirname "$0"`
+cd "$CURDIR"
+#CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # works in sourced files, only works for bash
+wget -O HGP.jpg -c http://humangenomeproject.yolasite.com/resources/HGPlogosm.jpg 
+convert HGP.jpg HGP.png
+mogrify -trim -transparent black -fuzz 5% HGP.png
+
+wget -c http://www.openworm.org/img/OpenWormLogo.png
+wget -c https://upload.wikimedia.org/wikipedia/commons/d/da/Galaxyzoo.jpg
+
+wget -c http://cdn.geekwire.com/wp-content/uploads/2014/10/alleninsitute12.jpg
+
+wget -O michael-nielsen.jpg -c 'http://michaelnielsen.org/images/mn.jpg'
+
+wget -O vaa3d-logo.png -c 'http://alleninstitute.org/media/filer_public/2a/9d/2a9d05db-8388-44b8-98c3-99204bdaf626/vaa3d_logo_small.jpg.250x272_q85.png'
