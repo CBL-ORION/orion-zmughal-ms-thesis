3c6d9587a9fd0c88f0ed53455c7bad3786c44527
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-12-09 13:46:20 -0600

    more presentation updates: add tables

--- gfx/present/go.sh
+++ gfx/present/go.sh
@@ -15,3 +15,7 @@ wget -c http://cdn.geekwire.com/wp-content/uploads/2014/10/alleninsitute12.jpg
 wget -O michael-nielsen.jpg -c 'http://michaelnielsen.org/images/mn.jpg'
 
 wget -O vaa3d-logo.png -c 'http://alleninstitute.org/media/filer_public/2a/9d/2a9d05db-8388-44b8-98c3-99204bdaf626/vaa3d_logo_small.jpg.250x272_q85.png'
+
+wget -O mathworks-logo.gif -c http://www.mathworks.com/cmsimages/64848_wl_cc_logo_membrane_2002_wl.gif
+convert mathworks-logo.gif mathworks-logo.png
+mogrify -trim -transparent white -fuzz 5% mathworks-logo.png
